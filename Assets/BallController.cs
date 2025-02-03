9260723c8597c252e8e19763e21f463aa8003c04
ThrackorZar <78045041+ThrackorZar@users.noreply.github.com>
2025-02-03 13:58:50 -0800

    added gutter on trigger detection

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -5,7 +5,6 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
 
-    // name booleans like a question
     private bool isBallLaunched;
     private Rigidbody ballRB;
     private InputManager inputManager;
@@ -19,11 +18,8 @@ public class BallController : MonoBehaviour
 
     private void LaunchBall()
     {
-        // now your if check can be framed like a sentence
-        // "if ball is launched, then simply return and do nothing"
         if (isBallLaunched) return;
         
-        // "now that the ball is not launched, set it to true and launch the ball"
         isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
